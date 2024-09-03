eaaa13cb72487db6edc0969a20ebca1560a98a73
Jakub Jakubiak <34916730+JakubJakubiak@users.noreply.github.com>
2024-09-03 18:10:21 +0200

    Title

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App: React.FC = () => {
   return (
     <Provider store={store}>
       <div className="App">
-        <h1>User Management Table</h1>
+        <h1>Building Table in Redux</h1>
         <UserTable />
       </div>
     </Provider>
